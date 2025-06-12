2cf12c5642505f9acd22d0c3b3a12c331b3bb31b
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-06-12 17:23:58 +0700

    jika posisi FRONT lebih kecil atau sama dengan REAR (antrian tidak melingkar)

--- Queues.cpp
+++ Queues.cpp
@@ -96,6 +96,13 @@ public:
             }
 
             FRONT_position = 0;
+
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
          }
     }
 };
