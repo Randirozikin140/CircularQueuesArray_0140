ad96eb43d6a1ed36dc4ae8ba87c303ef9fc258c6
Randi rozikin <randi.roziikin.ft24@mail.umy.ac.id>
2025-06-12 17:21:07 +0700

    menambahkan iterasi jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array

--- Queues.cpp
+++ Queues.cpp
@@ -87,5 +87,13 @@ public:
             }
             cout << endl;
         }
+         else{
+            //jika FRONT_position > REAR_position, iterasi dari FRONT hingga akhir array 
+            while (FRONT_position < max)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+         }
     }
 };
